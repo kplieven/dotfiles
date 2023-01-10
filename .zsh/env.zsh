43e97f7daab17c49f661c97069b313d6bf7d6a3f
Lievens, Karel <karel.lievens@barco.com>
2023-01-10 15:04:53 +0100

    replace powerlevel10k with starship and clean up .zshrc

---
+++ .zsh/env.zsh
@@ -0,0 +1,5 @@
+export PATH="$HOME/.local/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
