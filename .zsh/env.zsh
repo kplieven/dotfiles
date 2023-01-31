342ee295e23019bf7a86a97b34d58a9c80ff049f
kplieven <68231688+kplieven@users.noreply.github.com>
2023-01-31 10:20:03 +0100

    zsh (env): add LLVM_DIR, PROFILE_DIR and GTEST_COLOR

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -1,4 +1,11 @@
+LLVM_DIR=$HOME/Repositories/llvm-project/
+
+export PROFILE_DIR="$HOME/Brewery/w3phoenix/tools/w3phoenix-sdk"
+
+export GTEST_COLOR=yes
+
 export PATH="$HOME/.local/bin:$PATH"
+export PATH="$LLVM_DIR/build/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
