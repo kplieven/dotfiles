40143d2e817aaac50b194de9801589cc16e6c0c2
kplieven <68231688+kplieven@users.noreply.github.com>
2023-05-08 13:53:22 +0200

    zsh (env): add rust environment to ENV

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -10,3 +10,7 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source $HOME/.cargo/env
+
+# source "/etc/profile.d/rvm.sh"
