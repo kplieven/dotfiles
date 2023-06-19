ed39907afcc14f998f060efcec66c8586dd471cf
Lievens, Karel <karel.lievens@barco.com>
2023-06-19 09:00:06 +0200

    lvim: migrate to lazy.nvim for v0.9.0 upgrade

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -76,7 +76,6 @@ vim.opt.sw = 4
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
 lvim.builtin.alpha.mode = "dashboard"
-lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
@@ -381,7 +380,7 @@ lvim.plugins = {
     },
     {
         "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
+        build = function() vim.fn["mkdp#util#install"]() end,
     },
     {
         "aklt/plantuml-syntax"
