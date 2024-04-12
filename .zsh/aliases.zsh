7be391b30ded5da8d9bd9080a352a97938858916
Lievens, Karel <karel.lievens@barco.com>
2024-04-12 08:02:40 +0200

    zsh (aliases): remove docker command alias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -26,6 +26,7 @@ else
   export EDITOR='vim'
 fi
 
+
 # mimic vim functions
 alias :q='exit'
 alias :e='vim'
@@ -40,19 +41,3 @@ alias wtf='git commit -m "$(curl -s whatthecommit.com/index.txt)"'
 # Barco VPN
 alias vpnconnect='openconnect korvpn.barco.com/token --background --useragent "AnyConnect Windows 4.10.06079" -v'
 alias vpndisconnect='killall -SIGINT openconnect'
-
-# Barco Docker
-alias dockerrun='sudo docker run --user karlie --net host\
-    --mount type=bind,source="$(pwd)",target=/home/karlie/$(basename "$(pwd)") \
-    -v /home/karlie/Brewery/:/home/karlie/Brewery/ \
-    -v /home/karlie/.conan/:/home/karlie/.conan/ \
-    -v /home/karlie/.ssh/:/home/karlie/.ssh/ \
-    -v /home/karlie/.zsh/:/home/karlie/.zsh/ \
-    -v /home/karlie/.config/lvim/:/home/karlie/.config/lvim/ \
-    -v /home/karlie/.zshrc:/home/karlie/.zshrc \
-    -v /home/karlie/.gitconfig:/home/karlie/.gitconfig \
-    -v /home/karlie/.config/starship.toml:/home/karlie/.config/starship.toml \
-    --workdir /home/karlie/$(basename $(pwd)) \
-    -it '
-
-alias brewery-dev='ruby -I ~/projects/cs-brewery/lib ~/projects/cs-brewery/exe/cs-brewery'
