4b8aba031f9f5a9a8a008b27a540922d5d465316
kplieven <68231688+kplieven@users.noreply.github.com>
2024-09-18 15:52:49 +0200

    zsh (perf): disabled nvm on startup because it was too slow

--- .zsh/env.zsh
+++ .zsh/env.zsh
@@ -7,9 +7,9 @@ export GTEST_COLOR=yes
 export PATH="$HOME/.local/bin:$PATH"
 export PATH="$LLVM_DIR/build/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 export CDPATH=.:$HOME/Repositories/Barco
 
