201bdccaeda6e61c1107ccd4ed385dd4e9b626de
kplieven <68231688+kplieven@users.noreply.github.com>
2024-11-21 13:33:34 +0100

    scripts (dependencies): add lazygit

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -21,3 +21,10 @@ make -j `nproc` CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 
 nvim --headless "+Lazy! sync" +qa
+
+# lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+curl -Lo /tmp/lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit -D -t /usr/local/bin/
+rm lazygit
