f70be36c2690b4e99c4edec4441930445b6fca98
Lievens, Karel <karel.lievens@barco.com>
2025-01-29 16:56:29 +0100

    nvim: update CodeSnap.nvim configuration

--- .config/nvim/lua/custom/codesnap-setup.lua
+++ .config/nvim/lua/custom/codesnap-setup.lua
@@ -1,5 +1,11 @@
 require("codesnap").setup({
     watermark = "",
     save_path = "/home/karlie/Pictures/codesnap/test.png",
+    code_font_family = "JetBrainsMono Nerd Font",
     has_breadcrumbs = true,
+    bg_theme = "dusk",
+    bg_x_padding = 32,
+    bg_y_padding = 22,
+    has_line_number = true,
+    show_workspace = true,
 })
