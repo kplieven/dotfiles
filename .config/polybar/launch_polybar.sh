4eeead0e54853f2957c19e0aa7b3eed69a8ad945
Karel Lievens <lvnskarel@gmail.com>
2025-09-19 10:59:50 +0200

    polybar: rewrite config from scratch

--- .config/polybar/launch_polybar.sh
+++ .config/polybar/launch_polybar.sh
@@ -1,14 +1,11 @@
-# Terminate already running bar instances
 killall -q polybar
 
-# Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 0.2; done
 
 if type "xrandr"; then
-  for m in $(xrandr --listactivemonitors | grep -oP '(?<=\s)[a-zA-Z0-9-]+(?=$)' | tail -n +2); do
-    echo "launching on monitor $m"
-    MONITOR=$m polybar --config=/home/karlie/.config/polybar/config.ini --reload top-bar &
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload main &
   done
 else
-  polybar --config=/home/karlie/.config/polybar/config.ini --reload top-bar &
+  polybar --reload main &
 fi
